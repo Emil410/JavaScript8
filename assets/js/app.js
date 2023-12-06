f7d8a2f90e427820e782205a46417764616b529e
Emil410 <gasanovemil13@gmail.com>
2023-12-07 00:55:24 +0400

    six_exercise_done

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+// var sentence = prompt("Cumleni yaz!");
+// var words = sentence(' ');
+// if (words.length === 2) {
+//     alert("Cumlede ikidene soz var");
+// } else {
+//     alert("Cumlede ikidene soz yoxdur");
+// }
+//1
+// var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+// var passwordLength = 10;
+// var password = '';
+// for (var i = 0; i < passwordLength; i++) {
+//     var randomIndex = Math.floor(Math.random() * letters.length);
+//     password += letters[randomIndex];
+// }
+// console.log(password);
+// 2
+// var sentence = prompt("Cumle yaz!")
+// console.log(sentence.toUpperCase());
+// 3
+// var sentence = prompt("Cumle yaz!");
+// console.log(sentence.toLowerCase());
+// 4
+// var word = prompt("Cumleni yaz!");
+// var change = " ";
+// for (let i = 0; i < word.length; i++) {
+//     if (i === 0) {
+//         change = change + word[0].toUpperCase();
+//     } else {
+//         change = change + word[i].toLowerCase();
+//     }
+// }
+// console.log(change);
+// 5
+// var word = prompt("Cumle yaz!");
+// var change = " ";
+// for (let i = 0; i < word.length; i++) {
+//     if (i === word.length - 1) {
+//         change = change + word[i].toUpperCase();
+//     } else {
+//         change = change + word[i].toLowerCase();
+//     }
+// }
+// console.log(change);
+// 6
